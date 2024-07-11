871bead6da47984f9998ffafdff652037cd116a6
belyakova-anna <belyakova.anna.st@yandex.ru>
2024-07-11 19:25:25 +0300

    location text fix

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -3,20 +3,20 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import About from '../pages/index';
 
-describe('Компонент About', () => {
-  test('отображает изображение пользователя с корректным alt текстом', () => {
+describe('Component About', () => {
+  test('displays the users image with the correct alt text', () => {
     render(<About />);
     const userImageElement = screen.getByAltText('Image');
     expect(userImageElement).toBeInTheDocument();
   });
 
-  test('отображает информацию о местоположении с корректным текстом', () => {
+  test('displays location information with correct text', () => {
     render(<About />);
     const locationInfoElement = screen.getByText('Innopolis, Russia');
     expect(locationInfoElement).toBeInTheDocument();
   });
 
-  test('отображает компонент с информацией о пользователе', () => {
+  test('displays a component with user information', () => {
     render(<About />);
     const userInfoElement = screen.getByTestId('user-info');
     expect(userInfoElement).toBeInTheDocument();
--- components/LocationInfo.tsx
+++ components/LocationInfo.tsx
@@ -5,9 +5,15 @@ const locationIcon = '/img/icons/location.png'; // Путь до изображ�
 
 const LocationInfo: React.FC<{ location: string }> = ({ location }) => {
   return (
-    <div className="location-info">
-      <Image src={locationIcon} alt="Location Icon" width={20} height={20} />
-      <span>{location}</span>
+    <div className="location">
+      <Image
+        src={locationIcon}
+        alt="Location"
+        width={24}
+        height={24}
+        className="small-image"
+      />
+      <p>{location}</p>
     </div>
   );
 };

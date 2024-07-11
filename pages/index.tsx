b9764b3b3782102eebbad58665408fd90296955b
belyakova-anna <belyakova.anna.st@yandex.ru>
2024-07-11 12:32:45 +0300

    style check

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import LocationInfo from '../components/LocationInfo';
 import UserInfo from '../components/UserInfo';
 import Head from 'next/head';
 
+// kjhkh
 const About: React.FC = () => {
   return (
     <>
